6433f16299c0bf100736b3e917a9404967b9d0dc
Muhammad Ali <ali.mazhar@techno-soft.com>
2022-08-19 18:11:35 +0500

    Sidebar transofrm immplemented

--- javascript/jsfile.js
+++ javascript/jsfile.js
@@ -1,3 +1,4 @@
+document.getElementById("Tiles").addEventListener("click",SidebarClose)
 const windowsize=window.matchMedia("(max-width:800px)");
 windowsize.addEventListener("change",Resize);
 Resize(windowsize);
@@ -20,7 +21,29 @@ function SearchButton()
     document.getElementById("SearchSection").style.display="block";  
     document.getElementById("SearchSection").style.zIndex="1";
 }
-function Active(e)
+
+//Sidebar active li code
+// Get the container element
+var btnContainer = document.getElementById("SidebarUL");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("Item");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+function SidebarClose()
 {
-    e.style.backgroundColor= "#f50e0e";
+   if(document.getElementById("Sidebar").classList.contains("squeeze"))
+   {
+    document.getElementById("Sidebar").classList.remove("squeeze");
+   }
+   else{
+    document.getElementById("Sidebar").classList.add("squeeze");
+   }
 }

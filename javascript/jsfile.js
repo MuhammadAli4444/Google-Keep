a328d334e797fd5001b700e0b1d4448c3f15ba0d
Muhammad Ali <ali.mazhar@techno-soft.com>
2022-08-22 18:09:53 +0500

    Main section started

--- javascript/jsfile.js
+++ javascript/jsfile.js
@@ -1,16 +1,57 @@
-document.getElementById("Tiles").addEventListener("click",SidebarClose)
+document.getElementById("Tiles").addEventListener("click",SidebarClose);
+document.getElementById("Title").addEventListener("focus",ShowNoteSection);
+document.getElementById("Close").addEventListener("click",savedNotes);
+const box=document.querySelector(".MainBody");
+document.addEventListener("click",function(event){
+  if(event.target.closest(".MainBody")) return;
+  savedNotes()
+})
+function ShowNoteSection()
+{
+  document.getElementById("Title").placeholder='Add a Title';
+  const nodeList = document.querySelectorAll(".Footer");
+  for (let i = 0; i < nodeList.length; i++) {
+    nodeList[i].style.display="block";
+  }
+}
+function savedNotes()
+{
+  var title=document.getElementById("Title").value;
+  document.getElementById("Title").value="";
+  document.getElementById("Title").placeholder="Take a note...";
+  var Description=document.getElementById("Description").value;
+  document.getElementById("Description").value="";
+  document.getElementById("Description").placeholder=" Take a note...";
+  console.log(title);
+  localStorage.setItem(title,Description);
+  const nodeList = document.querySelectorAll(".Footer");
+  for (let i = 0; i < nodeList.length; i++) {
+    nodeList[i].style.display="none";
+  }
+}
 const windowsize=window.matchMedia("(max-width:800px)");
 windowsize.addEventListener("change",Resize);
 Resize(windowsize);
 function Resize(Argument_)
 { if(Argument_.matches)
     {
+      
        document.getElementById("SearchSection").style.display="none";
        document.getElementById("Searchh").style.display="block";
+       const nodeList = document.querySelectorAll(".Iconss");
+for (let i = 0; i < nodeList.length; i++) {
+  nodeList[i].style.display="none";
+
+}
+
        }
        else{
         document.getElementById("SearchSection").style.display="block";
         document.getElementById("Searchh").style.display="none";
+        const nodeList = document.querySelectorAll(".Iconss");
+        for (let i = 0; i < nodeList.length; i++) {
+          nodeList[i].style.display="block";
+        }
        }
 
 } 
@@ -47,3 +88,5 @@ function SidebarClose()
     document.getElementById("Sidebar").classList.add("squeeze");
    }
 }
+
+
